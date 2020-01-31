10782d869aebdb33cc25d74399596c9ed3acb60d
diyhome <diyhome@outlook.com>
2020-01-31 12:01:09 +0800

    Add:读取到知乎的Json文件

--- get_counter/get_text.py
+++ get_counter/get_text.py
@@ -5,3 +5,25 @@
 # @Site : https://gitee.com/jingjiangxueyuan_hmqs/
 # @File : get_text.py
 # @Software: PyCharm
+# 参数列表解释:https://luckymrwang.github.io/2019/04/06/知乎-API-v4-整理/
+
+from urllib import request
+
+def get_json(url):
+    # 截取问题id
+    qid = url.split('/')[-1]
+
+    # 安装知乎api v4规则拼接json的url
+    json_url = 'https://www.zhihu.com/api/v4/questions/{0}/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question.detail,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized;data[].mark_infos[].url;data[].author.follower_count,badge[].topics&limit=5&offset={1}&platform=desktop&sort_by=default'.format(qid, "5")
+
+    # 伪装浏览器头并且读取json文件
+    header = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
+    head = {}
+    head['User-Agent'] = header
+    rq = request.Request(json_url, headers=head)
+    res = request.urlopen(rq)
+    json = res.read().decode('utf-8')
+    return json
+
+if __name__ == '__main__':
+    get_json("https://www.zhihu.com/question/315755986")
