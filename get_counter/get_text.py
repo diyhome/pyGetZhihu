d43b010ca538716c856f1ba9a3adc2b13ad4ea95
diyhome <diyhome@outlook.com>
2020-01-31 12:14:55 +0800

    Add:logging

--- get_counter/get_text.py
+++ get_counter/get_text.py
@@ -8,6 +8,13 @@
 # 参数列表解释:https://luckymrwang.github.io/2019/04/06/知乎-API-v4-整理/
 
 from urllib import request
+import logging
+
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"    # 日志格式化输出
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"                        # 日期格式
+fp = logging.FileHandler('get_text.log', encoding='utf-8')
+fs = logging.StreamHandler()
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])    # 调用
 
 def get_json(url, offset=0):
     # 截取问题id
@@ -15,7 +22,7 @@ def get_json(url, offset=0):
 
     # 安装知乎api v4规则拼接json的url
     json_url = 'https://www.zhihu.com/api/v4/questions/{0}/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question.detail,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized;data[].mark_infos[].url;data[].author.follower_count,badge[].topics&limit=5&offset={1}&platform=desktop&sort_by=default'.format(qid, offset)
-
+    logging.info('json_url: ' + json_url)
     # 伪装浏览器头并且读取json文件
     header = 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19'
     head = {}
@@ -26,4 +33,5 @@ def get_json(url, offset=0):
     return json
 
 if __name__ == '__main__':
-    get_json("https://www.zhihu.com/question/315755986")
+    json_raw = get_json("https://www.zhihu.com/question/315755986")
+    print(json_raw)
