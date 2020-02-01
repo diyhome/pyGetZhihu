1675a10d0310dc9ef25ca74dc5641e75c54607fe
diyhome <diyhome@outlook.com>
2020-02-01 10:10:01 +0800

    Refactor:log记录文件改名

--- get_counter/get_text.py
+++ get_counter/get_text.py
@@ -13,7 +13,7 @@ import json
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"    # 日志格式化输出
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"                        # 日期格式
-fp = logging.FileHandler('get_text.log', encoding='utf-8')
+fp = logging.FileHandler('pyzhihu.log', encoding='utf-8')
 fs = logging.StreamHandler()
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])    # 调用
 
