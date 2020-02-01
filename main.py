666c0d22ce81e4016253af788c52b2a4c5e03185
diyhome <diyhome@outlook.com>
2020-02-01 13:42:40 +0800

    Add:结巴分词提取标题

--- main.py
+++ main.py
@@ -5,13 +5,33 @@
 # @Site : https://gitee.com/jingjiangxueyuan_hmqs/
 # @File : main.py
 # @Software: PyCharm
-from hal_data_clean import main
-from get_counter import get_text
+import jieba
+from rJson.g_json import GJson
+from HALDataClean import DataClean
+
 
 url = "https://www.zhihu.com/question/339011506"
+ListofInvaild = ['.', ',', '!', '。', '，', '！', '？', '“', '”', '"', '的', '你', '我', '他', '哪些', '她', '偏', '哪', '到', '觉得', '也', '有']
+
+def extract_keywords(title):
+    none_val = jieba.cut(title)
+    tmp_str = "/".join(none_val)
+    list_str = []
+    for keyword in tmp_str.split("/"):
+        if keyword in ListofInvaild:
+            continue
+        list_str.append(keyword)
+    return list_str
 
 if __name__ == '__main__':
-    raw_data = get_text.get_json(url)
-    for index in range(len(raw_data)):
-        tmp = main(raw_data[index][1])
-        print(tmp)
+    pgs = GJson(url)
+    dc = DataClean()
+
+    # sentence_list = []
+    # raw_data = pgs.get_json()
+    # for index in range(len(raw_data)):
+    #     tmp = dc.filter(raw_data[index][1])
+    #     if tmp == []:
+    #         continue
+    #     sentence_list += tmp
+    print(extract_keywords("有哪些气贯长虹的诗词？"))
