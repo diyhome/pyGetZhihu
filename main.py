9d14cf5c6abdb1694a9f4a470240a4772a2cf975
diyhome <diyhome@outlook.com>
2020-02-01 14:11:25 +0800

    Add:查询数据库分类,有则记录id,无则添加并记录id Add:logggin

--- main.py
+++ main.py
@@ -6,14 +6,22 @@
 # @File : main.py
 # @Software: PyCharm
 import jieba
-from rJson.g_json import GJson
 from HALDataClean import DataClean
+from rJson.g_json import GJson
+from db_opreate.op_sql import Database
+import logging
 
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"    # 日志格式化输出
+DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"                        # 日期格式
+fp = logging.FileHandler('pyzhihu.log', encoding='utf-8')
+fs = logging.StreamHandler()
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])    # 调用
 
 url = "https://www.zhihu.com/question/339011506"
-ListofInvaild = ['.', ',', '!', '。', '，', '！', '？', '“', '”', '"', '的', '你', '我', '他', '哪些', '她', '偏', '哪', '到', '觉得', '也', '有']
+ListofInvaild = ['.', ',', '!', '。', '，', '！', '？', '“', '”', '"', '的', '你', '我', '他', '哪些', '她', '偏', '哪', '到', '觉得', '也', '有', '是', '或者']
 
 def extract_keywords(title):
+    title = title.replace(' ', '')
     none_val = jieba.cut(title)
     tmp_str = "/".join(none_val)
     list_str = []
@@ -26,6 +34,9 @@ def extract_keywords(title):
 if __name__ == '__main__':
     pgs = GJson(url)
     dc = DataClean()
+    db = Database()
+
+    category_id_list = []
 
     # sentence_list = []
     # raw_data = pgs.get_json()
@@ -34,4 +45,18 @@ if __name__ == '__main__':
     #     if tmp == []:
     #         continue
     #     sentence_list += tmp
-    print(extract_keywords("有哪些气贯长虹的诗词？"))
+
+    # 数据库分类处理
+    keywords = extract_keywords("气贯长虹 唯美 诗词 这是")
+    for key in keywords:
+        db_ans = db.select_more("category", 'cname = "%s"' % key)
+        if not db_ans:
+            print("%s 不在数据库中" % key)
+            cid = db.count("category")
+            sql_op = dict()
+            sql_op['cid'] = '%s' % str(cid + 1)
+            sql_op['cname'] = '"%s"' % key
+            sql_op['count'] = '0'
+            category_id_list.append(cid + 1)
+        else:
+            category_id_list.append(int(db_ans[0]['cid']))
