8a105b45c7f4d7307d0680847009c1272a1498c7
diyhome <diyhome@outlook.com>
2020-02-02 12:44:00 +0800

    Update:数据库索引更新

--- zhihu.sql
+++ zhihu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- 主机： localhost
--- 生成日期： 2020-02-01 15:32:17
--- 服务器版本： 5.5.62-log
--- PHP 版本： 7.3.11
+-- 主机： 127.0.0.1
+-- 生成日期： 2020-02-02 05:08:15
+-- 服务器版本： 10.4.11-MariaDB
+-- PHP 版本： 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `category` (
   `cid` int(11) NOT NULL COMMENT '分类id',
-  `cname` text NOT NULL COMMENT '分类名',
-  `count` int(11) NOT NULL COMMENT '引用计数'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `cname` text DEFAULT NULL COMMENT '分类名',
+  `count` int(10) UNSIGNED DEFAULT NULL COMMENT '引用计数'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='所属分类';
 
 -- --------------------------------------------------------
 
@@ -41,10 +41,10 @@ CREATE TABLE `category` (
 --
 
 CREATE TABLE `press_sentence` (
-  `id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `sentence_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(11) NOT NULL COMMENT 'id',
+  `category_id` int(11) DEFAULT NULL COMMENT '分类键',
+  `sentence_id` int(11) DEFAULT NULL COMMENT '句子键'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='关联表';
 
 -- --------------------------------------------------------
 
@@ -54,11 +54,11 @@ CREATE TABLE `press_sentence` (
 
 CREATE TABLE `sentence` (
   `sid` int(11) NOT NULL COMMENT '句子id',
-  `content` longtext CHARACTER SET utf8 NOT NULL COMMENT '内容',
-  `howfrom` text NOT NULL COMMENT '出处',
-  `hot` int(11) NOT NULL COMMENT '热度',
-  `author` text NOT NULL COMMENT '作者'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `content` text DEFAULT NULL COMMENT '句子内容',
+  `howfrom` text DEFAULT NULL COMMENT '句子出处',
+  `hot` int(10) UNSIGNED NOT NULL COMMENT '句子热度',
+  `author` text DEFAULT NULL COMMENT '句子作者'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='句子记录';
 
 --
 -- 转储表的索引
@@ -68,31 +68,43 @@ CREATE TABLE `sentence` (
 -- 表的索引 `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`cid`);
+  ADD KEY `cid` (`cid`);
 
 --
 -- 表的索引 `press_sentence`
 --
 ALTER TABLE `press_sentence`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`),
-  ADD KEY `sentence_id` (`sentence_id`);
+  ADD KEY `id` (`id`),
+  ADD KEY `cid` (`category_id`),
+  ADD KEY `sid` (`sentence_id`);
 
 --
 -- 表的索引 `sentence`
 --
 ALTER TABLE `sentence`
-  ADD PRIMARY KEY (`sid`);
+  ADD KEY `sid` (`sid`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
 --
 
 --
+-- 使用表AUTO_INCREMENT `category`
+--
+ALTER TABLE `category`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类id';
+
+--
 -- 使用表AUTO_INCREMENT `press_sentence`
 --
 ALTER TABLE `press_sentence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- 使用表AUTO_INCREMENT `sentence`
+--
+ALTER TABLE `sentence`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT COMMENT '句子id';
 
 --
 -- 限制导出的表
@@ -102,8 +114,8 @@ ALTER TABLE `press_sentence`
 -- 限制表 `press_sentence`
 --
 ALTER TABLE `press_sentence`
-  ADD CONSTRAINT `press_sentence_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `press_sentence_ibfk_2` FOREIGN KEY (`sentence_id`) REFERENCES `sentence` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `cid` FOREIGN KEY (`category_id`) REFERENCES `category` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sid` FOREIGN KEY (`sentence_id`) REFERENCES `sentence` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
